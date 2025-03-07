2f37379f63e8bb638f33bf7d2ff3a30665088337
André Pimpão <a.pimpao@programmator.dev>
2025-03-07 18:39:17 +0000

    chore: create translator only once

--- src/Translator/Translator.php
+++ src/Translator/Translator.php
@@ -8,19 +8,22 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 readonly class Translator implements TranslatorInterface
 {
-    public function __construct(private string $locale) {}
+    private \Symfony\Component\Translation\Translator $translator;
 
-    public function trans(string $id, array $parameters = [], ?string $domain = null, ?string $locale = null): string
+    public function __construct(private string $locale)
     {
         // make sure we always have the correct path for the translation files
         $packagePath = InstalledVersions::getInstallPath('symfony/validator');
         $resourcePath = sprintf('%s/Resources/translations/validators.%s.xlf', $packagePath, $this->locale);
 
-        $translator = new \Symfony\Component\Translation\Translator($this->locale);
-        $translator->addLoader('xlf', new XliffFileLoader());
-        $translator->addResource('xlf', $resourcePath, $this->locale);
+        $this->translator = new \Symfony\Component\Translation\Translator($this->locale);
+        $this->translator->addLoader('xlf', new XliffFileLoader());
+        $this->translator->addResource('xlf', $resourcePath, $this->locale);
+    }
 
-        return $translator->trans($id, $parameters, $domain, $locale);
+    public function trans(string $id, array $parameters = [], ?string $domain = null, ?string $locale = null): string
+    {
+        return $this->translator->trans($id, $parameters, $domain, $locale);
     }
 
     public function getLocale(): string
